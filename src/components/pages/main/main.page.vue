9de3d06294f4481d259f4ff208a3741c29a19f82
Oleksandr Nikitin <a.nikitin@i.ua>
2023-12-23 18:53:14 +0200

    i18n

--- src/components/pages/main/main.page.vue
+++ src/components/pages/main/main.page.vue
@@ -1,22 +1,9 @@
 <template src="./main.page.html" lang="html"></template>
 
 <script>
-import { defineComponent } from 'vue'
-import { useI18n } from 'vue-i18n'
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: "main-page",
-  setup() {
-    const { t } = useI18n({
-      inheritLocale: true,
-      useScope: 'local'
-    })
-
-    // Something todo ..
-
-    return { t }
-  }
+  name: "main-page"
 });
 </script>
-
-<i18n src="./i18n/ru.json" locale="ru"></i18n>
--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,7 @@
 import { createI18n } from 'vue-i18n'
+// import en from './translations/en.json';
+// import ru from './translations/ru.json';
 
-/**
- * Load locale messages
- *
- * The loaded `JSON` locale messages is pre-compiled by `@intlify/vue-i18n-loader`, which is integrated into `vue-cli-plugin-i18n`.
- * See: https://github.com/intlify/vue-i18n-loader#rocket-i18n-resource-pre-compilation
- */
 function loadLocaleMessages() {
   const locales = require.context('./', true, /[A-Za-z0-9-_,\s]+\.json$/i);
   const messages: any = {};
@@ -20,7 +16,7 @@ function loadLocaleMessages() {
       console.log(locales(key));
       messages[locale] = locales(key);
     }
-  })
+  });
 
   console.log(messages);
   return messages
@@ -28,7 +24,13 @@ function loadLocaleMessages() {
 
 export const i18n = createI18n({
   legacy: false,
-  locale: process.env.VUE_APP_I18N_LOCALE || 'en',
-  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
+  locale: localStorage.getItem('language') || 'ru',
   messages: loadLocaleMessages()
 })
+//
+// export const i18n = createI18n({
+//   legacy: false,
+//   locale: process.env.VUE_APP_I18N_LOCALE || 'en',
+//   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
+//   messages: loadLocaleMessages()
+// })
