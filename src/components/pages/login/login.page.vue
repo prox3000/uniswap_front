6ad963ecd65e4ce2f4b7e5f937be1be492f566ad
Oleksandr Nikitin <a.nikitin@i.ua>
2023-12-26 17:28:44 +0200

    Login page

--- src/components/pages/login/login.page.vue
+++ src/components/pages/login/login.page.vue
@@ -9,3 +9,6 @@ export default defineComponent({
 </script>
 
 <style src="./login.page.sass" lang="sass"></style>
+
+<i18n src="./translations/ru.json" lang="json5" locale="ru"></i18n>
+<i18n src="./translations/en.json" lang="json5" locale="en"></i18n>
--- src/i18n.ts
+++ src/i18n.ts
@@ -1,36 +1,20 @@
-import { createI18n } from 'vue-i18n'
-// import en from './translations/en.json';
-// import ru from './translations/ru.json';
+import { createI18n } from 'vue-i18n';
 
 function loadLocaleMessages() {
-  const locales = require.context('./', true, /[A-Za-z0-9-_,\s]+\.json$/i);
+  const locales = require.context('./', true, /[A-Za-z0-9-_,\s]+\.json$/i)
   const messages: any = {};
   locales.keys().forEach(key => {
     const matched = key.match(/([A-Za-z0-9-_]+)\./i);
     if (matched && matched.length > 1) {
       const locale = matched[1];
-      console.log(matched);
-      console.log(locale);
-      console.log(locales);
-      console.log(key);
-      console.log(locales(key));
-      messages[locale] = locales(key);
+      messages[locale] = messages[locale] ? {...messages[locale] ,...locales(key)} : locales(key);
     }
   });
-
-  console.log(messages);
-  return messages
+  return messages;
 }
 
 export const i18n = createI18n({
   legacy: false,
   locale: localStorage.getItem('language') || 'ru',
   messages: loadLocaleMessages()
-})
-//
-// export const i18n = createI18n({
-//   legacy: false,
-//   locale: process.env.VUE_APP_I18N_LOCALE || 'en',
-//   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
-//   messages: loadLocaleMessages()
-// })
+});
